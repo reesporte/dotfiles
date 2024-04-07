8620b54f54eaa893cbe4c15bcf23f071f0d87950
reesporte <reesedporter@gmail.com>
2024-04-06 20:45:35 -0600

    try this instead

--- .config/nvim/after/ftplugin/go.lua
+++ .config/nvim/after/ftplugin/go.lua
@@ -3,7 +3,7 @@ if vim.fn.executable('gopls') == 1 then
     vim.lsp.start({
         name = 'gopls server',
         cmd = { 'gopls' },
-        root_patterns = { 'go.mod' },
+        root_dir = vim.fs.dirname(vim.fs.find({'go.mod'}, { upward = true })[1])
     })
 end
 
